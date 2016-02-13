e0d2ad8bc80d188fa4d2caccf1f28bfd158f8b46
pimpisa <pimpisa.predaswad@gmail.com>
2016-02-13 13:50:37 -0500

    Created toDoItems in ViewController

--- Sneakers/Sneakers/ViewController.swift
+++ Sneakers/Sneakers/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var toDoItems = [ToDoItems]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if toDoItems.count > 0{
+        
+            return
+        
+        }
+        toDoItems.append(ToDoItems(text: "Drink a big glass of water"))
+        toDoItems.append(ToDoItems(text: "Look at the blue paper"))
+        toDoItems.append(ToDoItems(text: "---------"))
+        toDoItems.append(ToDoItems(text: "rule the Web"))
+        toDoItems.append(ToDoItems(text: "buy a new iPhone"))
+        toDoItems.append(ToDoItems(text: "darn holes in socks"))
+        toDoItems.append(ToDoItems(text: "write this tutorial"))
+        toDoItems.append(ToDoItems(text: "master Swift"))
+        toDoItems.append(ToDoItems(text: "learn to draw"))
+        toDoItems.append(ToDoItems(text: "get more exercise"))
+        toDoItems.append(ToDoItems(text: "catch up with Mom"))
+        toDoItems.append(ToDoItems(text: "get a hair cut"))
     }
 
     override func didReceiveMemoryWarning() {
